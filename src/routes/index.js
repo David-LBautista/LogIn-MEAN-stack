64861deecda769315e683544ae4b82dff10e827c
David-LBautista <196w0007@itszongolica.edu.mx>
2019-12-31 14:06:15 -0600

    post sign in to get token using jwt

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ const jwt = require('jsonwebtoken');
 
 router.get('/', (req,res) => res.send('hello world'));
 
+// porceso de REGISTRO
 router.post('/signup', async (req, res) => {
     //obtenemos los datos de la peticion
     const { email, password } = req.body;
@@ -18,8 +19,22 @@ router.post('/signup', async (req, res) => {
     await newUser.save();
 
     const token = jwt.sign({ _id: newUser._id }, 'secretkey');
-    res.status(200).json({ token });
-    
+    res.status(200).json({ token }); 
+});
+
+router.post('/signin', async (req,res) => {
+
+    const { email, password } = req.body;
+    const user = await User.findOne({email});
+
+    //si no existe el usuario retornar estatus 401
+    if(!user) return res.status(401).send('The email doesnt exists');
+    //si la contraseña del usuario no es igual a la de la base de datos
+    if(user.password !== password) return res.status(401).send("Wrong password");
+
+    const token = jwt.sign({_id: user._id}, 'secretkey');
+     res.status(200).json({token});
+
 });
 
 module.exports = router;
