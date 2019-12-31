838ba136e45b15497f13a501bb8f37411c4e32d4
David-LBautista <196w0007@itszongolica.edu.mx>
2019-12-31 16:06:03 -0600

    restricted routes and function to verify token

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,10 +18,13 @@ router.post('/signup', async (req, res) => {
     //como el metodo save es asincrono se le agrega await y async a la peticion
     await newUser.save();
 
+    //al registrarse obtenemos el token
     const token = jwt.sign({ _id: newUser._id }, 'secretkey');
     res.status(200).json({ token }); 
 });
 
+
+// to sign in: get the token
 router.post('/signin', async (req,res) => {
 
     const { email, password } = req.body;
@@ -34,7 +37,84 @@ router.post('/signin', async (req,res) => {
 
     const token = jwt.sign({_id: user._id}, 'secretkey');
      res.status(200).json({token});
-
 });
+
+//rutas para devolver datos publicos
+router.get('/tasks', (req,res) => {
+    res.json([
+        {
+        _id: 1,
+        name: 'Task one',
+        description: 'Lorem ipsum',
+        date: "2019-12-31T19:48:28.315Z"
+        },
+        {
+            _id: 2,
+            name: 'Task two',
+            description: 'Lorem ipsum',
+            date: "2019-12-31T19:48:28.315Z"
+            },
+            {
+                _id: 3,
+                name: 'Task three',
+                description: 'Lorem ipsum',
+                date: "2019-12-31T19:48:28.315Z"
+                },
+    ]);
+});
+
+
+//ruta para datos privados
+router.get('/private-tasks', verifyToken, (req,res) => {
+    res.json([
+        {
+        _id: 01,
+        name: 'private one',
+        description: 'Lorem ipsum',
+        date: "2019-12-31T19:48:28.315Z"
+        },
+        {
+            _id: 02,
+            name: 'private two',
+            description: 'Lorem ipsum',
+            date: "2019-12-31T19:48:28.315Z"
+            },
+            {
+                _id: 03,
+                name: 'private three',
+                description: 'Lorem ipsum',
+                date: "2019-12-31T19:48:28.315Z"
+                },
+    ]);
+});
+
+
+
+
+
+
+
+
+
+//funcion de validacion del token
+function verifyToken( req, res, next) {
+
+    //si no existen los headers
+    if(!req.headers.authorization){
+        return res.status(401).send("Unauthorized request");
+    }
+    //Si viene una cabecera pero
+    //comprobemos el token
+    const token = req.headers.authorization.split(' ')[1];
+    if(token === null){
+        return res.status(401).send("Unauthorized request");
+    }
+
+    const payload = jwt.verify(token, 'secretkey');
+    console.log(payload);
+
+    req.userId = payload._id;
+    next();
+}
 
 module.exports = router;
